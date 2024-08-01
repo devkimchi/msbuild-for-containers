2125e012d441c826f617da19c7b0cde8c9ef1d63
Justin Yoo <justin.yoojh@gmail.com>
2024-08-01 12:52:18 +0900

    Initial code commit (#1)

---
+++ MSBuildForContainers.WebApp/ApiClients/ApiAppClient.cs
@@ -0,0 +1,20 @@
+﻿using MSBuildForContainers.WebApp.Models;
+
+namespace MSBuildForContainers.WebApp.ApiClients;
+
+public interface IApiAppClient
+{
+    Task<List<WeatherForecast>> GetWeatherForecastAsync();
+}
+
+public class ApiAppClient(HttpClient http) : IApiAppClient
+{
+    private readonly HttpClient _http = http ?? throw new ArgumentNullException(nameof(http));
+
+    public async Task<List<WeatherForecast>> GetWeatherForecastAsync()
+    {
+        var forecasts = await _http.GetFromJsonAsync<List<WeatherForecast>>("weatherforecast").ConfigureAwait(false);
+
+        return forecasts ?? [];
+    }
+}
